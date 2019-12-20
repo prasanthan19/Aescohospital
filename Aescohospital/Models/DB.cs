354eec0302c15a1d090cda47c3fd2d3b1a5f242c
Prasanthan <prasanthan1995@gmail.com>
2019-12-21 01:33:25 +0530

    new change

---
+++ Aescohospital/Models/DB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Aescohospital.Models
+{
+    public static class DB
+    {
+        private static string connectionstring = @"Data Source=DESKTOP-MQ51FIV\SQLEXPRESS;Initial Catalog=Aesco_db;Integrated Security=True";
+
+
+        public static void Executewithoutreturn(string procedureName, DynamicParameters param)
+        {
+            using 
+        }
+    }
+}
---
+++ Aescohospital/Models/PatientModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aescohospital.Models
+{
+    public class PatientModel
+    {
+        public int patientID { get; set; }
+        public string pfname { get; set; }
+        public string plname { get; set; }
+        public string pnic { get; set; }
+        public int pmobno { get; set; }
+        public string paddress { get; set; }
+       
+
+    }
+}
